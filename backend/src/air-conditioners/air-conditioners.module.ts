9b95cc42c2635bcc8ad749e1157f3d60cbd53db1
Dat-Nicolas <topchit031@gmail.com>
2026-05-14 00:12:37 +0700

    fix1

--- backend/src/air-conditioners/air-conditioners.module.ts
+++ backend/src/air-conditioners/air-conditioners.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AirConditionersService } from './air-conditioners.service';
 import { AirConditionersController } from './air-conditioners.controller';
+import { RoomsModule } from '../rooms/rooms.module';
 
 @Module({
+  imports: [RoomsModule],
   providers: [AirConditionersService],
-  controllers: [AirConditionersController]
+  controllers: [AirConditionersController],
 })
 export class AirConditionersModule {}
